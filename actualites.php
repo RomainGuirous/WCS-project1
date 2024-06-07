e8f2163fa35e28ac9e6fd7ab870d54a6aa8e3b00
Idellette <idellettesom@yahoo.fr>
2024-06-07 10:55:58 +0200

    extension.php

---
+++ actualites.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="actualites.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <h1 class="logo"><a href="index.html">ZC</a></h1>
+            <h3 class="link"> <a href="expositions.php"> Expositions</a></h3>
+            <h3 class="link"> <a href="oeuvres.php">Oeuvres</a></h3>
+            <h3 class=" link last-element"> <a href="actualites.html">Actualités</a></h3>
+        </div>
+    </header>
+    
+    <main>
+            <?php require('data.php'); ?>
+        <h1>Actualités</h1>
+        <div class="actualites-container">
+            <?php foreach ($actualites as $infosArray) : ?>
+                <article>
+                    <img src="<?= $infosArray['image'] ?>" alt=<?= $infosArray['title'] ?>>
+                    <p>
+                        <?= $infosArray['description'] ?>
+                    </p>
+                </article>
+            <?php endforeach ?>  
+        </div>    
+    
+    </main>
+
+    <footer>
+            <div class="author">
+                <ul class="list-wihout-dot">
+                <li>Romain Guirous</li>
+                <li>Dorian Dequirez</li>
+                <li>Hermine Som Idellette Judith</li>
+            </ul></div>
+            <div class="navigation">
+                <ul class="list-wihout-dot">
+                    <li><a href="index.html">ZC</a></li>
+                    <li><a href="expositions.php"> Expositions</a></li>
+                    <li><a href="oeuvres.html">Oeuvres</a></li>
+                    <li><a href="actualites.php">Actualités</a></li>
+                </ul>
+            </div>
+    </footer>
+</body>
+</html>
